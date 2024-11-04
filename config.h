423b198258be38b6a6a423d6ecb0e872be3dceb8
zach <partsguy83@gmail.com>
2024-11-03 19:17:42 -0600

    intial commit

--- config.h
+++ config.h
@@ -8,8 +8,8 @@ static const unsigned int gappx     = 8;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=14" };
-static const char dmenufont[]       = "monospace:size=14";
+static const char *fonts[]          = { "Ubuntu Mono:size=14" };
+static const char dmenufont[]       = "Ubuntu Mono:size=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -62,15 +62,17 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
-static const char *volumeUp[] = { "wpctl",  "set-volume", "@DEFAULT_AUDIO_SINK@", "5%+", NULL };
-static const char *volumeDown[] = {"wpctl", "set-volume", "@DEFAULT_AUDIO_SINK@", "5%-", NULL };
+static const char *mutecmd[]  = { "amixer", "-q", "set", "Master", "toggle", NULL };
+static const char *volumeupcmd[] = { "amixer", "-q", "set", "Master", "5%+", "unmute", NULL };
+static const char *volumedowncmd[] = {"amixer", "-q", "set", "Master", "5%-", "unmute", NULL };
 
 
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ 0,                             XF86XK_AudioRaiseVolume, spawn, {.v = volumeUp } },
-	{ 0,                             XF86XK_AudioLowerVolume, spawn, {.v = volumeDown } },
+	{ 0,                            XF86XK_AudioMute,        spawn, {.v = mutecmd } }, 
+	{ 0,                            XF86XK_AudioRaiseVolume, spawn, {.v = volumeupcmd } },
+	{ 0,                            XF86XK_AudioLowerVolume, spawn, {.v = volumedowncmd } },
 	{ MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
@@ -106,7 +108,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY,                       XK_q,      quit,           {0} },
+	{ MODKEY,                       XK_q,      quit,           {1} },
 };
 
 /* button definitions */
